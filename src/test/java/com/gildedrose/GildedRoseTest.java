d473005bd4c383f01d90c248fb70c8e35914528d
abarhub <abarhub@github.com>
2018-09-08 11:55:55 +0200

    amelioration des tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,21 @@ import org.junit.Test;
 public class GildedRoseTest {
 
 	@Test
-	public void foo() {
+	public void testFoo() {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		assertEquals("fixme", app.items[0].name);
+		assertEquals("foo", app.items[0].name);
+	}
+
+	@Test
+	public void testSimple() {
+		Item[] items = new Item[] { new Item("pomme", 10, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("pomme", app.items[0].name);
+		assertEquals(9, app.items[0].sellIn);
+		assertEquals(19, app.items[0].quality);
 	}
 
 }
