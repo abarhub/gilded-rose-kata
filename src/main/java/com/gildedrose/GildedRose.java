873554442d07e9346529d40e70b12bf5aab9a94c
abarhub <abarhub@github.com>
2018-09-08 13:23:34 +0200

    factorisation du code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,85 +1,83 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final String ITEM_AGED_BRIE = "Aged Brie";
-    private static final String ITEM_BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros";
-    Item[] items;
+	private static final String ITEM_AGED_BRIE = "Aged Brie";
+	private static final String ITEM_BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros";
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            updateQuality(items[i]);
-        }
-    }
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			updateQuality(items[i]);
+		}
+	}
 
-    private void updateQuality(Item item) {
-        if (!equals(item, ITEM_AGED_BRIE)
-                && !equals(item, ITEM_BACKSTAGE_PASSES)) {
-            if (item.quality > 0) {
-                if (!equals(item, ITEM_SULFURAS)) {
-                    decrementQuality(item);
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                incrementQuality(item);
+	private void updateQuality(Item item) {
+		if (equals(item, ITEM_SULFURAS)) {
+			//on ne fait rien
+		} else {
+			if (!equals(item, ITEM_AGED_BRIE)
+					&& !equals(item, ITEM_BACKSTAGE_PASSES)) {
+				if (item.quality > 0) {
+					decrementQuality(item);
+				}
+			} else {
+				if (item.quality < 50) {
+					incrementQuality(item);
 
-                if (equals(item, ITEM_BACKSTAGE_PASSES)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            incrementQuality(item);
-                        }
-                    }
+					if (equals(item, ITEM_BACKSTAGE_PASSES)) {
+						if (item.sellIn < 11) {
+							incrementQualityIfPossible(item);
+						}
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            incrementQuality(item);
-                        }
-                    }
-                }
-            }
-        }
+						if (item.sellIn < 6) {
+							incrementQualityIfPossible(item);
+						}
+					}
+				}
+			}
 
-        if (!equals(item, ITEM_SULFURAS)) {
-            decrementSellIn(item);
-        }
+			decrementSellIn(item);
 
-        if (item.sellIn < 0) {
-            if (!equals(item, ITEM_AGED_BRIE)) {
-                if (!equals(item, ITEM_BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
-                        if (!equals(item, ITEM_SULFURAS)) {
-                            decrementQuality(item);
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    incrementQuality(item);
-                }
-            }
-        }
-    }
+			if (item.sellIn < 0) {
+				if (!equals(item, ITEM_AGED_BRIE)) {
+					if (!equals(item, ITEM_BACKSTAGE_PASSES)) {
+						if (item.quality > 0) {
+							decrementQuality(item);
+						}
+					} else {
+						item.quality = 0;
+					}
+				} else {
+					incrementQualityIfPossible(item);
+				}
+			}
+		}
+	}
 
-    private boolean equals(Item item, String itemBackstagePasses) {
-        return item.name.equals(itemBackstagePasses);
-    }
+	private void incrementQualityIfPossible(Item item) {
+		if (item.quality < 50) {
+			incrementQuality(item);
+		}
+	}
 
-    private void decrementSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
+	private boolean equals(Item item, String itemBackstagePasses) {
+		return item.name.equals(itemBackstagePasses);
+	}
 
-    private void decrementQuality(Item item) {
-        item.quality = item.quality - 1;
-    }
+	private void decrementSellIn(Item item) {
+		item.sellIn = item.sellIn - 1;
+	}
 
-    private void incrementQuality(Item item) {
-        item.quality = item.quality + 1;
-    }
+	private void decrementQuality(Item item) {
+		item.quality = item.quality - 1;
+	}
+
+	private void incrementQuality(Item item) {
+		item.quality = item.quality + 1;
+	}
 }
